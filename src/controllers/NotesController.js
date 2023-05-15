6b22a0c1d1a00a97815208b4495c0ed418d30378
gabriel <gabrielbarroncas1@gmail.com>
2023-05-15 10:37:07 +0100

    order notes by tags

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -59,12 +59,22 @@ class NotesConroller{
     }
 
     async index(request, response){
-        const {user_id} = request.query
+        const {title, user_id, tags} = request.query
 
-        const notes = await knex("notes")
-        .where({user_id}) 
-        .orderBy("title")
+        let notes
 
+        if (tags){
+            const filterTags = tags.split(',').map(tag => tag.trim())
+
+            notes = await knex("tags")
+            .whereIn("name", filterTags)
+
+        } else {
+            notes = await knex("notes")
+            .where({user_id})
+            .whereLike("title", `%${title}%`) 
+            .orderBy("title")
+        }
         return response.json(notes)
     }
 }
