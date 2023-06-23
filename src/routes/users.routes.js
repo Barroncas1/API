1b84ba55c9e6ab40f98875c22c1bc274bd2e1d31
Gabriel <gabrielbarroncas1@gmail.com>
2023-06-23 08:45:16 -0300

    finish the middleware authentication

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express")
 
 const UserController = require("../controllers/UsersController")
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const userRoutes = Router()
 
 const usersController = new UserController()
 
 userRoutes.post("/", usersController.create);
-userRoutes.put("/:id", usersController.update)
+userRoutes.put("/", ensureAuthenticated, usersController.update)
 
 module.exports = userRoutes
